89a27ca566b56dd23f61bd7ac0dd03c5ee0e3a2b
MihajloPetrovicDev <petrovicmihajlo789@gmail.com>
2024-12-15 22:53:41 +0100

    Add content to Home component Add functionality for displaying user info in MyAccount component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
     <PageHeader />
+
+    <div>
+        <h1 class="text-center mt-80 fw-bold">Your first choice for cloud storage.</h1>
+
+        <div id="home-info-container">
+            <p class="text-center mx-auto">Your first choice for secure, convenient, and reliable cloud storage. Whether you’re storing documents, photos, or music, <b>SefCloud</b> makes it easy to keep your files safe and accessible from anywhere. With our simple interface and powerful features, managing your cloud storage has never been easier.</p>
+            <ul class="text-center mx-auto mt-4">
+                <li><b>User-Friendly Interface:</b> Easily upload, organize, and access your files.</li>
+                <li><b>Anywhere Access:</b> Access your files from any device with an internet connection.</li>
+            </ul>
+            <p class="text-center mx-auto mb-0 mt-4">Join <b>SefCloud</b> today and start organizing your digital life effortlessly!</p>    
+        </div>
+    </div>
 </template>
 
 <script>
@@ -12,3 +25,24 @@ export default {
     } 
 }
 </script>
+
+<style scoped>
+h1 {
+    font-size: 4rem;
+}
+
+ul {
+    width: 30vw;
+}
+
+#home-info-container {
+    width: 50vw;
+    height: fit-content;
+    margin-inline: auto;
+    background-color: white;
+    padding: 50px;
+    margin-top: 70px;
+    border-radius: 10px;
+    border: 2px solid #e7e7e7;
+}
+</style>
--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,9 +1,20 @@
 <template>
     <MinimalHeader />
+
+    <div class="d-flex">
     <MyAccountSideNavBar />
+
+    <div class="mx-auto mt-80">
+        <p class="fs-1">Username: &nbsp;<b>{{ user.userName }}</b></p>
+        <p class="fs-1 mt-4">Email: &nbsp;<b>{{ user.email }}</b></p>
+    </div>
+    </div>
 </template>
 
+
+
 <script>
+import axios from 'axios';
 import MinimalHeader from '../components/MinimalHeader.vue';
 import MyAccountSideNavBar from '../components/MyAccountSideNavBar.vue';
 
@@ -12,6 +23,28 @@ export default {
     components: {
         MinimalHeader,
         MyAccountSideNavBar
+    },
+    methods: {
+        async getUserInfo() {
+            const response = await axios.get(`https://localhost:7169/api/auth/get-user-info`, 
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('auth-token')}`,
+                }
+            });
+
+            if(response.data.success) {
+                this.user = response.data.user;
+            }
+        }
+    },
+    data() {
+        return {
+            user: ""
+        }
+    },
+    mounted() {
+        this.getUserInfo()
     }
 }
 </script>
